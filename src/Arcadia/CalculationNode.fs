c577aa21ecba9c2eeaeecc528903a53503ddf7cf
Dave Carlson <mndrake22@mac.com>
2014-03-08 13:14:37 -0600

    added calculation node shell

---
+++ src/Arcadia/CalculationNode.fs
@@ -0,0 +1,15 @@
+﻿namespace Arcadia
+open System
+
+type CalculationNode<'T>() as this =
+    inherit CalculationEngine()
+    do ()
+
+    let mutable dependentNodes : INode<'T> option = None
+    let mutable outNode : INode<'T> option = None
+
+    interface INode<'T> with
+
+        member I.Value = 
+
+
---
+++ src/Samples/FSharpApp/Models/CollectionCalculationEngine.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharpApp.Models
+
+open System.Collections.Generic
+open System.Threading
+open Arcadia
+open System
+
+type CollectionCalculationEngine() as this =
+    inherit CalculationEngine()
+
+    do        
+        // helper functions to add input/output nodes
+        let inline input x = this.AddInput x
+        let inline output nodes f = this.AddOutput(nodes, NodeFunc(f))
+
+        // input nodes
+        let in0 = input 1
+
+        ()
+
